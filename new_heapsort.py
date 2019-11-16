c58e675953544095a75d49e33cd8e0bc266c30c8
greykoshak <greykoshak@gmail.com>
2019-11-17 00:48:01 +0200

    heapsort - свой вариант, работает

--- new_heapsort.py
+++ new_heapsort.py
@@ -7,16 +7,16 @@ def add_element(heap, x):
         p //= 2
 
 
-def del_element(ptr_heap, heap_len):
-    ptr_heap[heap_len], ptr_heap[0] = ptr_heap[0], ptr_heap[heap_len]
-    n = heap_len - 1
+def del_element(ptr_heap):
+    ptr_heap[0] = ptr_heap.pop()
+    n = len(ptr_heap) - 1
     p = 0
     moved = True
 
-    while moved and p * 2 <= n:
+    while moved and (p * 2 + 1) <= n:
         moved = False
         max = p * 2 + 1
-        if (max + 1) and ptr_heap[max + 1] > ptr_heap[max]:
+        if (max + 1 <= n) and ptr_heap[max + 1] > ptr_heap[max]:
             max += 1
         if ptr_heap[p] < ptr_heap[max]:
             ptr_heap[p], ptr_heap[max] = ptr_heap[max], ptr_heap[p]
@@ -31,6 +31,9 @@ arr = [12, 11, 13, 5, 6, 7, 3, 7, 14, 1, 4, 1, 73, 4, 17, 3, 21, 81]
 for i in range(0, len(arr)):
     add_element(heap, arr[i])
 print("heap: ", heap)
-for i in range(len(heap)-1, -1, -1):
-    del_element(heap, i)
-print("sorted: ", heap)
+
+sorted = []
+for i in range(len(heap)-1, 0, -1):
+    sorted.append(heap[0])
+    del_element(heap)
+print("sorted: ", sorted, "\n", sorted[::-1])
