b1251eda317372e97add4685f06fe426b50903fd
greykoshak <greykoshak@gmail.com>
2019-11-17 21:00:23 +0200

    Отладка heapsort_new "Проверка на дорогах"

--- new_heapsort.py
+++ new_heapsort.py
@@ -1,3 +1,6 @@
+import random
+
+
 def add_element(heap, x):
     heap.append(x)
     p = len(heap) - 1
@@ -25,7 +28,8 @@ def del_element(ptr_heap):
 
 
 heap = []
-arr = [12, 11, 13, 5, 6, 7, 3, 7, 14, 1, 4, 1, 73, 4, 17, 3, 21, 81]
+# arr = [12, 11, 13, 5, 6, 7, 3, 7, 14, 1, 4, 1, 73, 4, 17, 3, 21, 81]
+arr = [random.randint(1, 100) for i in range(150)]
 
 # Heap creation
 for i in range(0, len(arr)):
@@ -37,3 +41,12 @@ for i in range(len(heap) - 1, 0, -1):
     sorted.append(heap[0])
     del_element(heap)
 print("sorted: ", sorted, "\n", sorted[::-1])
+
+print("Проверка на дорогах...")
+fl = True
+for i in range(len(sorted) - 1):
+    if sorted[i] < sorted[i + 1]:
+        print("Error: {}, {}".format(sorted[i], sorted[i + 1]))
+        fl = False
+print("Чисто") if fl else print("Не пройден!")
+
